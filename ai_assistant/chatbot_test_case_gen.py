71ed0eeb10fac73de2661ce6c32e1b3512a6bd7e
Sanoj Swaminathan <53048769+sanoj-s@users.noreply.github.com>
2024-11-28 18:53:30 +0530

    Update chatbot_test_case_gen.py

--- ai_assistant/chatbot_test_case_gen.py
+++ ai_assistant/chatbot_test_case_gen.py
@@ -51,6 +51,7 @@ st.markdown(
 # Page setup
 st.logo("./bot.png")
 st.title("Generate your manual test cases...")
+st.text("Bot can make mistakes. Review the response.")
 
 # Initialize the model
 llm = ChatOpenAI(model="gpt-3.5-turbo")
