9d4f90cbc9cc977c6ca6caf06d79b63d3a213fc9
Sanoj Swaminathan <53048769+sanoj-s@users.noreply.github.com>
2024-12-02 17:46:43 +0530

    Update chatbot.py

--- ai_assistant/chatbot.py
+++ ai_assistant/chatbot.py
@@ -63,7 +63,7 @@ bot_icon_base64 = get_image_as_base64("./bot.png")
 st.markdown(
     f"""
     <div style="display: flex; align-items: center; gap: 10px;">
-        <img src="data:image/png;base64,{bot_icon_base64}" alt="Bot Icon" style="border-radius: 50%; width: 40px; height: 40px;">
+        <img src="data:image/png;base64,{bot_icon_base64}" alt="Bot Icon" style="border-radius: 50%; width: 60px; height: 60px;">
         <h1 style="margin: 0;">I'm here to help you...</h1>
     </div>
     """,
