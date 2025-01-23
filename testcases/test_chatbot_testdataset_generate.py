e7ea9f16aa95fb03d81fef1426de15f477d2f97e
sanoj-arch <sanoj.swaminathan@aspiresys.com>
2025-01-23 09:42:34 +0530

    updated

--- testcases/test_chatbot_testdataset_generate.py
+++
@@ -1,8 +0,0 @@
-from data_generation.chatbot_data_generate import generate_from_web_urls
-
-
-def test_chatbot_app():
-    generate_from_web_urls(
-        'https://www.aspiresys.com/newsroom/awards-and-recognition/aspire-systems-ceo-gowri-shankar-subramanian-among'
-        '-indias-most',
-        4)
